0add110e725a486cb13efd17dfafdab239729c2a
Exclusion-QX <soulproxy0404@gmail.com>
2021-11-15 22:27:51 +0300

    Add component alert

--- resources/views/admin/tasks/create.blade.php
+++ resources/views/admin/tasks/create.blade.php
@@ -12,10 +12,7 @@
                 </div>
             </div>
             @if (session('success'))
-                <div class="alert alert-success" role="alert">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
-                    <h4><i class="fa fa-check"></i>{{ session('success') }}</h4>
-                </div>
+                <x-status.success />
             @endif
             @if ($errors->any())
                 <div class="alert alert-danger">
---
+++ resources/views/components/status/success.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-success" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+    <h4><i class="fa fa-check"></i>{{ session('success') }}</h4>
+</div>
