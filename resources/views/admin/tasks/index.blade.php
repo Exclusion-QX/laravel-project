0eb88a52f892471dc77754110df6136d94eba74e
Exclusion-QX <soulproxy0404@gmail.com>
2021-11-15 22:25:26 +0300

    Add tab to task table

--- resources/views/admin/tasks/index.blade.php
+++ resources/views/admin/tasks/index.blade.php
@@ -12,10 +12,7 @@
                 </div>
             </div>
             @if (session('success'))
-                <div class="alert alert-success" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
-                    <h4><i class="fa fa-check"></i>{{ session('success') }}</h4>
-                </div>
+                <x-status.success />
             @endif
         </div>
     </div>
@@ -28,25 +25,31 @@
                         <div class="card-header p-0 border-bottom-0">
                             <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
                                 <li class="nav-item">
-                                    <a class="nav-link active" id="custom-tabs-four-home-tab" data-toggle="pill" href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home" aria-selected="true">
+                                    <a class="nav-link active" id="custom-tabs-new-tab" data-toggle="pill" href="#custom-tabs-new" role="tab" aria-controls="custom-tabs-new" aria-selected="true">
                                         Новые
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" id="custom-tabs-four-profile-tab" data-toggle="pill" href="#custom-tabs-four-profile" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">
+                                    <a class="nav-link" id="custom-tabs-inwork-tab" data-toggle="pill" href="#custom-tabs-inwork" role="tab" aria-controls="custom-tabs-inwork" aria-selected="false">
                                         В работе
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill" href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">
+                                    <a class="nav-link" id="custom-tabs-completed-tab" data-toggle="pill" href="#custom-tabs-completed" role="tab" aria-controls="custom-tabs-completed" aria-selected="false">
                                         Завершенные
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="custom-tabs-all-tab" data-toggle="pill" href="#custom-tabs-all" role="tab" aria-controls="custom-tabs-all" aria-selected="false">
+                                        Все
                                     </a>
                                 </li>
                             </ul>
                         </div>
                         <div class="card-body">
                             <div class="tab-content" id="custom-tabs-four-tabContent">
-                                <div class="tab-pane fade active show" id="custom-tabs-four-home" role="tabpanel" aria-labelledby="custom-tabs-four-home-tab">
+                                {{--Статус Новая--}}
+                                <div class="tab-pane fade active show" id="custom-tabs-new" role="tabpanel" aria-labelledby="custom-tabs-new-tab">
                                     <div class="col-lg-12">
                                         <div class="card">
                                             <div class="card-body p-0">
@@ -65,7 +68,8 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel" aria-labelledby="custom-tabs-four-profile-tab">
+                                {{--Статус В работе--}}
+                                <div class="tab-pane fade" id="custom-tabs-inwork" role="tabpanel" aria-labelledby="custom-tabs-inwork-tab">
                                     <div class="col-lg-12">
                                         <div class="card">
                                             <div class="card-body p-0">
@@ -84,7 +88,8 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="custom-tabs-four-messages" role="tabpanel" aria-labelledby="custom-tabs-four-messages-tab">
+                                {{--Статус Завершена--}}
+                                <div class="tab-pane fade" id="custom-tabs-completed" role="tabpanel" aria-labelledby="custom-tabs-completed-tab">
                                     <div class="col-lg-12">
                                         <div class="card">
                                             <div class="card-body p-0">
@@ -101,7 +106,26 @@
                                             </div>
                                             <!-- /.card-body -->
                                         </div>
-                                    </div>                                </div>
+                                    </div>
+                                </div>
+                                {{--Все задачи--}}
+                                <div class="tab-pane fade" id="custom-tabs-all" role="tabpanel" aria-labelledby="custom-tabs-all-tab">
+                                    <div class="col-lg-12">
+                                        <div class="card">
+                                            <div class="card-body p-0">
+                                                <table class="table table-striped projects">
+                                                    <x-tables.head_tasks/>
+                                                    <tbody>
+                                                    @foreach($tasks as $task)
+                                                        <x-tables.tasks id="{{ $task['id'] }}" title="{{ $task['title'] }}" description="{{ $task['description'] }}" created="{{ $task['created_at'] }}" deadline="{{ $task['deadline'] }}" priority="{{ $task['priority'] }}" status="{{ $task['status'] }}" />
+                                                    @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <!-- /.card-body -->
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <!-- /.card -->
