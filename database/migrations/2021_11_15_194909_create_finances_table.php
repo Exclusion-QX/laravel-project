9ffd14ffd71ea4792840d28cda010b40ee66fb85
Exclusion-QX <soulproxy0404@gmail.com>
2021-11-18 15:12:40 +0300

    Add migrations

---
+++ database/migrations/2021_11_15_194909_create_finances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFinancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finances', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->timestamps();
+            $table->float('income')->nullable();;
+            $table->float('forced_expenses')->nullable();;
+            $table->float('expenses')->nullable();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finances');
+    }
+}
